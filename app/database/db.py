fb491f33426e824b22a1d6b2eda015dd2a345f73
karom-ai <karomatullodzhuraev@gmail.com>
2025-08-08 06:33:10 +0500

    Correct

--- app/database/db.py
+++ app/database/db.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
 
+# Временное решение - используем прямое подключение к SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./tickets.db"
 
 engine = create_engine(
@@ -12,7 +14,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
